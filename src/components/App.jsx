60ad6acd04fa5844fd52b030f511d4edf9332763
kramer1985 <necib.yassine.tux@gmail.com>
2025-03-03 17:18:53 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+  return (
+    <div className="container">
+      <h1>Hello </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
