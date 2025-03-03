ea5407df1a57d9de771ae9309a748ca202df2408
Yn <necib.yassine.tux@gmail.com>
2025-03-03 22:23:31 +0000

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
   function handleChange(event) {
     console.log(event.target.value);
+    setName(event.target.value);
   }
   return (
     <div className="container">
-      <h1>Hello </h1>
+      <h1>Hello {name}</h1>
       <input
         onChange={handleChange}
         type="text"
